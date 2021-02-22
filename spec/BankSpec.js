a8ecefe7ef91388fc5cab1a0c4bce1a93ff4e27e
Ovy95 <jackoverton21@hotmail.com>
2021-02-22 09:53:42 +0000

    first commit

---
+++ spec/BankSpec.js
@@ -0,0 +1,27 @@
+describe("Bank Class", function() {
+  var bank;
+
+  beforeEach (function () {
+   bank = new Bank();
+  });
+
+  describe('', function() {
+      it (" ",function() {
+      })
+    
+  })
+
+
+
+ 
+
+
+   
+
+
+  
+
+
+  
+
+});
