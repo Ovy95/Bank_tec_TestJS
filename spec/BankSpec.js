ee927e6f1139462de7b065c4ef600cacbb27b31f
Ovy95 <jackoverton21@hotmail.com>
2021-02-22 10:40:28 +0000

    debit method added

--- spec/BankSpec.js
+++ spec/BankSpec.js
@@ -24,6 +24,20 @@ describe("Bank Class", function() {
   
 })
 
+describe('debit method ', function() {
+  beforeEach(function () {
+    bank.credit(100)
+    bank.debit(50)
+  })
+  it ("Checks balance from beforeEach function called in spec",function() {
+    expect(bank.balance).toEqual(50)
+  })
+  it ("Checks statement has the transaction",function() {
+    expect(bank.print_statement()).toEqual(['date || credit || debit || balance','Date || 100 ||  || 100','Date ||  || 50 || 50'] )
+  })
+
+})
+
 
 
  
--- src/bank.js
+++ src/bank.js
@@ -2,16 +2,19 @@ class Bank {
 
   transactions = ["date || credit || debit || balance"]
   balance = 0
+  
   print_statement(){
     return this.transactions
   }
 
   credit(total){
     this.balance += total 
-    this.transactions.push("Date"+ " || " + total + " || "+ " || "+ this.balance)
+    this.transactions.push("Date"+ " || " + total + " || "+ " || " + this.balance)
+  }
 
-
-
+  debit(total){
+    this.balance -= total 
+    this.transactions.push("Date"+ " || "  + " || " + total + " || " + this.balance)
   }
 
 
