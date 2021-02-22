5ea58f0fffe7403e64105ffea86300dfe3ec95f9
Ovy95 <jackoverton21@hotmail.com>
2021-02-22 11:32:24 +0000

    Date added to debit and credit functions

--- spec/BankSpec.js
+++ spec/BankSpec.js
@@ -14,12 +14,13 @@ describe("Bank Class", function() {
   describe('Credit method', function() {
     beforeEach(function () {
       bank.credit(5)
+      date = bank.date()
     })
     it ("Adds to the balance",function() {
       expect(bank.balance).toEqual(5)
     })
     it ("Checks statement has the transaction",function() {
-      expect(bank.print_statement()).toEqual(['date || credit || debit || balance', 'Date || 5 ||  || 5'] )
+      expect(bank.print_statement()).toEqual(['date || credit || debit || balance', ("" + date +' || 5 ||  || 5')] )
     })
   
 })
@@ -28,12 +29,15 @@ describe('debit method ', function() {
   beforeEach(function () {
     bank.credit(100)
     bank.debit(50)
+    date = bank.date()
   })
   it ("Checks balance from beforeEach function called in spec",function() {
     expect(bank.balance).toEqual(50)
   })
   it ("Checks statement has the transaction",function() {
-    expect(bank.print_statement()).toEqual(['date || credit || debit || balance','Date || 100 ||  || 100','Date ||  || 50 || 50'] )
+    expect(bank.print_statement()).toEqual(['date || credit || debit || balance',("" + date +" || 100 ||  || 100"),("" + date +" ||  || 50 || 50")] )
+  
+  
   })
 
 })
--- src/bank.js
+++ src/bank.js
@@ -2,19 +2,33 @@ class Bank {
 
   transactions = ["date || credit || debit || balance"]
   balance = 0
-  
+
   print_statement(){
     return this.transactions
   }
 
   credit(total){
+    var date = this.date()
     this.balance += total 
-    this.transactions.push("Date"+ " || " + total + " || "+ " || " + this.balance)
+    this.transactions.push(date+ " || " + total + " || "+ " || " + this.balance)
   }
 
   debit(total){
+    var date = this.date()
     this.balance -= total 
-    this.transactions.push("Date"+ " || "  + " || " + total + " || " + this.balance)
+    this.transactions.push(date+ " || "  + " || " + total + " || " + this.balance)
+  }
+
+  date(){
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+    today = dd + '/' + mm + '/' + yyyy;
+
+    var date_formatted = today.toString()
+
+    return date_formatted
   }
 
 
