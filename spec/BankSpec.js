1f0dd7e87312f0970806c27700a0ed95c11b4be4
Ovy95 <jackoverton21@hotmail.com>
2021-02-24 11:32:45 +0000

    Added Tests to check dates

--- spec/BankSpec.js
+++ spec/BankSpec.js
@@ -7,7 +7,7 @@ describe("Bank Class", function() {
   });
 
   describe('Print_statement method', function() {
-      it ("Returns an empty statement",function() {
+      it ("Returns empty transactions ",function() {
         expect(bank.print_statement()).toEqual('date || credit || debit || balance');
       })
     
@@ -31,11 +31,12 @@ describe("Bank Class", function() {
       bank.credit(100.00)
       bank.debit(50.00)
       date = bank.get_date()
+  
     })
     it ("Checks balance from beforeEach function called in spec",function() {
       expect(bank.balance).toEqual(50.00)
     })
-    it ("Checks statement has the transaction",function() {
+    it ("Checks statement has the 2 transactions passed into it in order",function() {
       expect(bank.print_statement()).toEqual('date || credit || debit || balance\r\n'+("" + date +" || || 50.00 || 50.00\r\n")+("" + date +" || 100.00 || || 100.00") )
     })
 
@@ -58,6 +59,25 @@ describe("Bank Class", function() {
     })
     it ("Matches the order In print statements",function() {
       expect(bank.print_statement()).toEqual('date || credit || debit || balance\r\n'+date+' || || 500.00 || 2500.00\r\n'+date+' || 2000.00 || || 3000.00\r\n'+date+' || 1000.00 || || 1000.00')
+    })
+  })
+
+  describe('TESTING Get Date method', function() {
+    beforeEach(function () {
+      date = bank.get_date()
+    })
+    it ("Matches this date 2020,10,5",function() {
+      //                      (January = 0) 
+      expect(bank.get_date(new Date(2020,04,5))).toEqual('05/05/2020') 
+    })
+
+    it ("Prints out a statement for 5th of May",function() {
+      bank.credit(100,'22/02/2021')
+      bank.credit(100,'23/02/2021')
+      bank.debit(50,'23/02/2021')
+      bank.debit(50)
+      console.log(bank.print_statement())
+      expect(bank.print_statement()).toEqual('date || credit || debit || balance\r\n'+date+' || || 50.00 || 100.00\r\n'+'23/02/2021 || || 50.00 || 150.00\r\n'+'23/02/2021 || 100.00 || || 200.00\r\n'+'22/02/2021 || 100.00 || || 100.00')
     })
   })
 
--- src/bank.js
+++ src/bank.js
@@ -2,17 +2,16 @@ class Bank {
 
   transactions = ["date || credit || debit || balance"]
   balance = 0.00
-
-  
+  date = this.get_date()
   print_statement(){
     return this.transactions.join("\r\n")
   }
 
-  credit(total){
+  credit(total,date){
     this.balance += total 
-
+    if (date==null){
     var date = this.get_date()
-
+    }
     var current_balance = this.balance
     current_balance = this.int_to_strfloat(current_balance)
     total = this.int_to_strfloat(total)
@@ -20,30 +19,32 @@ class Bank {
     this.transactions.splice(1,0,(date+ " || " + total + " ||"+ " || " + current_balance ))
   }
 
-  debit(total){
+  debit(total,date){
     if(total > this.balance) {
       throw new Error("Insufficient Funds in the account");
     }
+    if (date==null){
+      var date = this.get_date()
+    }
+
     this.balance -= total 
-
-    var date = this.get_date()
-
+    
     var current_balance = this.balance
+
     current_balance = this.int_to_strfloat(current_balance)
     total = this.int_to_strfloat(total)
+    
     this.transactions.splice(1,0,(date+ " ||"  + " || " + total + " || " + current_balance))
   }
 
-  get_date(){
-    var today = new Date();
+  get_date(today = new Date()){
     var dd = String(today.getDate()).padStart(2, '0');
     var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
     var yyyy = today.getFullYear();
     today = dd + '/' + mm + '/' + yyyy;
 
-    var date_formatted = today.toString()
-
-    return date_formatted
+    today = today.toString()
+    return today
   }
 
   int_to_strfloat(number){
