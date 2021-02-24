beb670eddd45c7e2621010e2539ac2cd11168f28
Ovy95 <jackoverton21@hotmail.com>
2021-02-24 23:01:57 +0000

    Extracted the Date method and created tests

---
+++ spec/DateSpec.js
@@ -0,0 +1,32 @@
+describe("DATES Class", function() {
+  var dates;
+
+ beforeEach (function () {
+  dates = new Dates()
+ });
+
+ describe('Passing in Credit then Debit to check printStatement method is correct ', function() {
+   beforeEach(function () {
+     date = dates.get_date()
+   })
+   
+   it ("CHECKS THE DATE MATCHES IT",function() {
+     expect(dates.get_date()).toEqual(date)
+   })
+ })
+
+
+ describe('TESTING Get Date method', function() {
+  beforeEach(function () {
+         // January Starts from 0↓
+    my_birthday = new Date(2020,04,05)
+ })
+   it ("Prints out a custom date entered",function() {
+     expect(dates.get_date(my_birthday)).toEqual("05/05/2020")
+   })
+ })
+
+
+
+
+})
---
+++ src/date.js
@@ -0,0 +1,14 @@
+class Dates {
+
+  get_date(today = new Date()){
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+    today = dd + '/' + mm + '/' + yyyy;
+
+    today = today.toString()
+
+    return today
+  }
+
+}
